3b7c6640fda9a3e0ec848c7cb9bc4da2dd3390c3
arupsk <arjun.hs@gmail.com>
2025-05-19 08:08:28 +0300

    Update ForegroundService.java

--- app/src/main/java/com/Andriod/ER/com/ForegroundService.java
+++ app/src/main/java/com/Andriod/ER/com/ForegroundService.java
@@ -3,95 +3,48 @@ package com.Andriod.ER.com;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
-
 import com.Andriod.ER.R;
 
-import static android.content.ContentValues.TAG;
-
-@RequiresApi(api = Build.VERSION_CODES.O)
+/* loaded from: classes.dex */
 public class ForegroundService extends Service {
+    static final /* synthetic */ boolean $assertionsDisabled = false;
+    private static final String CHANNEL_DEFAULT_IMPORTANCE = "0xffffffff";
 
-    private static final String CHANNEL_DEFAULT_IMPORTANCE = "0xffffffff" ;
-    private void startMyOwnForeground(){
-        String NOTIFICATION_CHANNEL_ID = "com.example.simpleapp";
-        String channelName = "My Background Service";
-        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_LOW);
-        chan.setLightColor(Color.BLUE);
-        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        assert manager != null;
-        manager.createNotificationChannel(chan);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID).setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE);;
-        Notification notification = notificationBuilder.setOngoing(true)
-                .setSmallIcon(R.drawable.logo_er_smal)
-                .setContentTitle("App is running in background")
-                .setPriority(NotificationManager.IMPORTANCE_MIN)
-                .setCategory(Notification.CATEGORY_SERVICE)
-                .build();
-        startForeground(2, notification);
+    private void startMyOwnForeground() {
+        NotificationChannel notificationChannel = new NotificationChannel("com.example.simpleapp", "My Background Service", NotificationManager.IMPORTANCE_NONE);
+        notificationChannel.setLightColor(-16776961);
+        notificationChannel.setLockscreenVisibility(0);
+        ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(notificationChannel);
+        startForeground(2, new NotificationCompat.Builder(this, "com.example.simpleapp").setOngoing(true).setSmallIcon(R.drawable.logo_er_smal).setContentTitle("App is running in background").setPriority(1).setCategory(NotificationCompat.CATEGORY_SERVICE).build());
     }
 
-    @Override
+    @Override // android.app.Service
     public IBinder onBind(Intent intent) {
-        Log.i(TAG, "onBINDGround");
+        Log.i("ContentValues", "onBINDGround");
         return null;
     }
-    @Override
+
+    @Override // android.app.Service
     public void onCreate() {
-//        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
-        Log.i(TAG, "onCreatGround");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        Log.i("ContentValues", "onCreatGround");
+        if (Build.VERSION.SDK_INT >= 26) {
             startMyOwnForeground();
-        else
+        } else {
             startForeground(1, new Notification());
+        }
     }
 
-
-
-   /* @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(TAG, "onStartCommandGround");
-        Bitmap icon = BitmapFactory.decodeResource(getResources(),
-                R.drawable.logo_er_smal);
-       // Intent notificationIntent = new Intent(this, DeviceScanActivity.class);
-
-        Toast.makeText(this,"Creating Notification",Toast.LENGTH_SHORT).show();
-       // PendingIntent pendingIntent =
-           //     PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        Notification notification =
-                new Notification.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
-                        .setContentTitle("Energy Research")
-                        .setTicker("Bluetooth")
-                        .setContentText("Using Bluetooth..")
-                        .setSmallIcon(R.drawable.logo_er_smal)
-                        .setLargeIcon(Bitmap.createScaledBitmap(icon, 128, 128, false))
-                        .setOngoing(true)
-                        .build();
-
-        startForeground(1001,notification);
-        return START_REDELIVER_INTENT;
-    }*/
-    @Override
+    @Override // android.app.Service
     public void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "onDestroyGround");
-//        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
+        Log.i("ContentValues", "onDestroyGround");
         stopForeground(true);
     }
 }
